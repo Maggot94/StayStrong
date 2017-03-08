b4df430ea6b03f33cfa907d9b05882bd8166f3d0
Usuario-PC\Usuario <andrescastropuerta@hotmail.com>
2017-03-08 18:40:33 -0500

    Prototipo

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour {
+
+	public int vida = 100; 
+	private Animator Ataques; 
+
+	public float TimeAttack = 8f; 
+	public float stay = 10f; 
+
+	public bool Idamge = false; 
+	public bool NextAttackBool = false; 
+
+	public int StateAttack;
+
+	//----- parte roger---
+	public GameObject enemy;
+
+	public Transform spawnPoint;
+
+	private float spawner = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+
+		Ataques = GetComponent<Animator> (); 
+
+	
+	
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (NextAttackBool == false) {
+
+
+			stay -= Time.deltaTime;
+		}
+		if (stay <= 8f) {
+			Idamge = false; 
+			Ataques.SetInteger ("Attack", 0); 
+			TimeAttack -= Time.deltaTime; 
+		}
+
+		if (TimeAttack <= 0) {
+			
+			stay = 10f; 
+			NextAttackBool = true; 
+		}
+			
+		if( NextAttackBool == true)
+		{
+			StateAttack = Random.Range (0, 3);
+			Attack (StateAttack); 
+		}
+		
+	}
+
+	public void Attack (int a) {
+		
+		if (a == 1) 
+		{
+
+			Ataques.SetInteger ("Attack", a); 
+			Idamge = true; 
+			NextAttackBool = false; 
+			TimeAttack = 8f;
+
+		}
+
+		if (a == 2) 
+		{
+			Ataques.SetInteger ("Attack", a); 
+			Idamge = true; 
+			NextAttackBool = false; 
+			TimeAttack = 8f;
+		}
+
+
+	}
+
+	void OnMouseOver()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			vida = vida - 10;
+			//Debug.Log("vida - 1");
+		}
+		if (vida <= 0)
+		{
+			Invoke("Spawner", spawner);
+			Destroy(gameObject);
+		}
+	}
+
+	void Spawner()
+	{
+		Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+	}
+}
+
+
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Player : MonoBehaviour {
+
+	public int life = 100;
+	public int damage = 10;
+	public Image vida; 
+	public float maxTime; 
+	public float minSwipeDist;
+	public int direcction; 
+	public bool defense = false; 
+	public float defensetime = 2f;
+
+	private Animator Dodge; 
+
+	public Enemigo ene; 
+
+	float startTime; 
+	float endTime;
+
+	Vector3 starPos;
+	Vector3 endPos; 
+
+	float SwipeDistance; 
+	float SwipeTime;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		Dodge = GetComponent<Animator> (); 
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (defense == true) {
+
+			defensetime -= Time.deltaTime;
+
+			if (defensetime <= 0) {
+
+				Dodge.SetInteger ("Dodge", 0);
+				defensetime = 2f;
+				defense = false;
+
+			}
+		}
+		if (ene.Idamge == true && defense == false) {
+
+			life = life - 10; 
+			ene.Idamge = false; 
+		} 
+		if (Input.touchCount > 0) 
+		{
+			Touch touch = Input.GetTouch (0);
+
+			if (touch.phase == TouchPhase.Began) 
+			{
+				startTime = Time.time;
+				starPos = touch.position; 
+			}
+
+			else if (touch.phase == TouchPhase.Ended)
+			{
+				endTime = Time.time;
+				endPos = touch.position;
+
+				SwipeDistance = (endPos - starPos).magnitude;
+				SwipeTime = endTime - startTime;
+				if (SwipeTime < maxTime && SwipeDistance > minSwipeDist) {
+
+					Swipe ();
+
+				}
+	         }
+          }
+	   }
+
+	void Swipe ()
+	{
+		Vector2 distance = endPos - starPos;
+		if (Mathf.Abs(distance.x) > Mathf.Abs(distance.y) )
+		{
+			Debug.Log ("Horizonal Swipe");
+			if (distance.x > 0) {
+				Debug.Log ("Defensa Derecha"); 
+
+				Dodge.SetInteger ("Dodge", 1);
+				defense = true; 
+			}
+			if (distance.x < 0) {
+				Debug.Log ("Defensa Izquierda"); 
+				Dodge.SetInteger ("Dodge", 2);
+				defense = true; 
+				//Instantiate (Block, SpawnPoint.position, SpawnPoint.rotation);
+			}
+		}
+    }
+}
